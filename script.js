196a7efb3838cb6d283d7e41cb9c685777fca852
Yurik21S25 <site.owner.20@gmail.com>
2025-08-19 00:19:21 +0300

    test

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.body.innerHTML = '<h1>Hello from JavsScript</h1>'
 console.log('test 2');
 console.log('master + tests branch');
+<<<<<<< HEAD
+=======
+console.log('Hi! master + tests branch' + 'true');
+>>>>>>> tests
